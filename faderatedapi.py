70c24f275489b2c86302feed2f5ebddef7feb033
Qazi Khalid Amin <qkamin.ist@gmail.com>
2025-12-28 03:12:22 +0500

    Update faderatedapi.py

--- faderatedapi.py
+++ faderatedapi.py
@@ -24,7 +24,7 @@ def doc_helper(doc):
 # Endpoint to get document by ID
 @app.get("/documents/{doc_id}")
 async def get_document(doc_id: str):
-    data_path = ".../federated-mlflow-pipeline/brain-tumor-mri-dataset/"
+    data_path = "https://github.com/Amin-0513/federated-mlflow-pipeline/tree/main/brain-tumor-mri-dataset"
     
     # Validate ObjectId
     try:
